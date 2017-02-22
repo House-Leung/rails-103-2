c5a7379ea37ff521b745ddb00a95c3d82483f072
House_Leung <House.liangshihao@gmail.com>
2017-02-22 22:27:34 +0800

    implement edit/delete action on other people way

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_filter :authenticate_user!, :only => [:new, :create]
+  before_filter :authenticate_user!, :only => [:new, :create, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -18,25 +18,39 @@ class PostsController < ApplicationController
     else
       render :new
     end
-    
+
     def edit
       @group = Group.find(params[:group_id])
       #对照group，多了以上这行
       @post = Post.find(params[:id])
+      @post.group = @group
     end
 
     def update
       @group = Group.find(params[:group_id])
       #对照group，多出了以上这行，为什么？
       @post = Post.find(params[:id])
+      #对照论坛来
+      @post.group = @group
+      @post.user = current_user
 
       #在助教@mike 的帮助下，增加了if－else语句，
+      #为什么是 account_posts_path ?而不是 group_path(@group)
       if @post.update(post_params)
-        redirect_to group_path(@group), notice: "Update Success"
+        redirect_to account_posts_path, notice: "Update Success"
       else
       # 为什么是加上这个？
         render :edit
       end
+    end
+
+    def destroy
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      @post.group = @group
+      @post.destroy
+      flash[:alert] = "Group deleted"
+      redirect_to account_posts_path
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,11 @@ Rails.application.routes.draw do
 
   namespace :account do
     resources :groups
-    resources :posts 
+    resources :posts
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'groups#index'
+  resources :posts
 end
